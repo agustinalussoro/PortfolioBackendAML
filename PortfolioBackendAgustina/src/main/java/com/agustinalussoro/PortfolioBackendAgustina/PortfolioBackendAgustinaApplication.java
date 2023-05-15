7d5a0df8f926404836105c3649b0843e43710687
agustinalussoro <agustinalussoro@gmail.com>
2023-05-15 12:31:55 -0300

    commit

---
+++ PortfolioBackendAgustina/src/main/java/com/agustinalussoro/PortfolioBackendAgustina/PortfolioBackendAgustinaApplication.java
@@ -0,0 +1,13 @@
+package com.agustinalussoro.PortfolioBackendAgustina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBackendAgustinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackendAgustinaApplication.class, args);
+	}
+
+}
---
+++ PortfolioBackendAgustina/src/test/java/com/agustinalussoro/PortfolioBackendAgustina/PortfolioBackendAgustinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustinalussoro.PortfolioBackendAgustina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendAgustinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
